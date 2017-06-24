0dea8e27bd95cfaa9c346e7cbdb8be81ab41e329
David Cady Jr <davidcadyjr@mac.com>
2017-06-24 13:20:44 -0500

    server working:

---
+++ server/app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var server = require("http").createServer(app);
+var path = require('path');
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+
+server.listen(3000, function(){
+	console.log("listening on port 3000");
+})
