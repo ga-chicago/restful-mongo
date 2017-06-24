ef1d2c5d5d97cb41530b165f2c63de9cf0c62551
David Cady Jr <davidcadyjr@mac.com>
2017-06-24 13:48:10 -0500

    trying to get db running

--- server/app.js
+++ server/app.js
@@ -7,6 +7,12 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get('/', function(request, response){
+
+	console.log("hello");
+	
+})
+
 server.listen(3000, function(){
 	console.log("listening on port 3000");
 })
