56f6ab4d4bcf6339e9b7f64c963e06de7c05a5be
Sam Rolfe <scrolfe@gmail.com>
2017-06-25 12:04:49 -0500

    I hadn't placed the static file in app.js! Up and running, error with Author - need to figure out why Author input isn't working

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ var express = require('express'),
     require('./db/db')
 
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(express.static(path.join(__dirname, 'public'))); //public files will always start from public
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs')
