482a0daa8012afb22e2160b7278f47f44bb8a2f4
lindseygilmore <lindseygilmore21@gmail.com>
2017-06-25 18:18:12 -0500

    restful api framework added

--- server/app.js
+++ server/app.js
@@ -3,10 +3,41 @@ var express = require('express'),
 	server 	= require('http').createServer(app),
 	path 	= require('path'),
 	bodyParser = require('body-parser'),
-	Restaurant 	= require('./models/Restaurant.js');
+	Sushi 	= require('./models/Sushi.js');
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.get('/test', function(request, response){
     response.send('testing 123');
+})
+
+//get all sushi
+app.get('/sushi/', function(request, response){
+     //return all the sushi
+})
+
+//get sushi by id
+app.get('/sushi/:id', function(request, response){
+     //return a specific sushi roll by id
+ })
+
+//post /sushi --> use ajax function to actually post it
+app.post('/sushi', function(request, response){
+      //create a new sushi from the request body
+})
+
+//patch /wines/:id
+app.patch('/sushi/:id', function(request, response){
+      //update sushi id with the request body
+})
+
+//delete /sushi/:id
+app.delete('/sushi/:id', function(request, response){
+      //delete the sushi by id
 })
 
 
