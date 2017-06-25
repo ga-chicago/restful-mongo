6fc23dd102c2d1df71e9b5c4881c9261a4364251
lindseygilmore <lindseygilmore21@gmail.com>
2017-06-25 18:45:22 -0500

    updated restful framework

--- server/app.js
+++ server/app.js
@@ -11,41 +11,67 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+//first test once the server was listening
+// app.get('/test', function(request, response){
+//     response.send('testing 123');
+// })
 
-app.get('/test', function(request, response){
-    response.send('testing 123');
+//get 1 all sushi
+app.get('/', function(request, response){
+	Sushi.find(function(err, sushiRoll){
+		var allSushi = {sushiRoll: sushi};
+		response.render('home', allSushi);
+	})
 })
 
-//get all sushi
-app.get('/sushi/', function(request, response){
-     //return all the sushi
-     console.log('successget');
-})
-
-//get sushi by id
+//get 2 sushi by id
 app.get('/sushi/:id', function(request, response){
      //return a specific sushi roll by id
-     console.log('successgetid');
+     var id = request.params.id;
+     Sushi.findById(id, function(err, sushiRoll){
+     	response.json(sushi);
+     })
  })
 
 //post /sushi --> use ajax function to actually post it
 app.post('/sushi', function(request, response){
       //create a new sushi from the request body
-      var sushi = request.body.sushi
+      console.log(request.body)
+
+      var sushi = new Sushi ({name: request.body.name,
+      	type: request.body.type,
+      	vegetarian: request.body.vegetarian,
+      	cost: request.body.cost})
+
       response.json('success');
-      console.log('successpost');
+     
 })
 
 //patch /wines/:id
 app.patch('/sushi/:id', function(request, response){
       //update sushi id with the request body
-      response.json('success');
-      console.log('successpatch');
+      var id = request.params.id;
+      Sushi.findById(id, function(err, sushiRoll){
+      	sushi.name = request.body.name;
+		sushi.type = request.body.type;
+		sushi.vegetarian = request.body.vegetarian;
+		sushi.cost = request.body.cost;
+      })
+
+      sushi.save();
+
+      response.json(sushi);
+
 })
 
 //delete /sushi/:id
 app.delete('/sushi/:id', function(request, response){
       //delete the sushi by id
+      var id = request.params.id;
+      Sushi.findById(id, function(err, sushiRoll){
+      	sushi.remove();
+      	response.json('success');
+      })
 
 })
 
@@ -54,3 +80,17 @@ app.delete('/sushi/:id', function(request, response){
 server.listen(3000, function(){
 	console.log('listening on port 3000')
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
