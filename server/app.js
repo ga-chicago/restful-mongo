c6d296bc4fa6e9496d476d77285541602199528d
Sam Rolfe <scrolfe@gmail.com>
2017-06-25 11:50:31 -0500

    added input fields to home.hbs

--- server/app.js
+++ server/app.js
@@ -27,6 +27,48 @@ app.get('/books', function(req, res){
     }); //mongoose version of db.books.find() in repl
 })
 
+// get /books/:id
+app.get('/books/:id', function(req, res){
+    var id = req.params.id;
+    Book.findById(id, function(err, book){
+        console.log(id);
+        res.json(book);
+    });
+})
+// post /books
+app.post('/books', function(req, res){
+    console.log(req.body);
+var book = new Book ({name: req.body.name,
+               type: req.body.type,
+               region: req.body.region,
+               vintage: req.body.vintage,
+               organic: req.body.organic})
+    book.save();
+    res.send('success');
+})
+// patch /books/:id
+app.patch('/books/:id', function(req, res){
+    var id = req.params.id;
+    Book.findById(id, function(err, book){
+        book.name = req.body.name;
+        book.type = req.body.type;
+        book.region = req.body.region;
+        book.vintage = req.body.vintage;
+        book.organic = req.body.organic;
+        book.save();
+        res.json(book);
+    })
+})
+
+// delete /books/:id
+app.delete('/books/:id', function(req, res){
+    var id = req.params.id;
+    Book.findById(id, function(err, book){
+        book.remove();
+        res.json("success")
+    })
+})
+
 
 server.listen(3000, function(){
     console.log("Server running on port 3000")
