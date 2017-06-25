328b003c98a0a7b0e6f48f62de70159c83f105d8
Sam Rolfe <scrolfe@gmail.com>
2017-06-25 10:01:51 -0500

    set up Book model and connected to app.js

--- server/app.js
+++ server/app.js
@@ -1,4 +1,10 @@
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
+    server = require('http').createServer(app),
+    path = require('path')
+    Book = require('./models/Book.js')
+
+    require('.db/db')
+
     
---
+++ server/models/Book.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose')
+
+var BookSchema = new mongoose.Schema({
+    about: String,
+    numberOfPages: Number,
+    author: String,
+    datePublished: Number
+    genre: String
+})
+
+var bookModel = mongoose.model('Book', BookSchema);
+
+module.exports = bookModel;
