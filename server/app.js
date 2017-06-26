41bcfaca4a9d353aca3cc7d5074f49e2406fff40
lindseygilmore <lindseygilmore21@gmail.com>
2017-06-25 19:27:17 -0500

    post function working

--- server/app.js
+++ server/app.js
@@ -23,21 +23,21 @@ app.set('view engine', 'hbs');
 
 //get 1 all sushi
 app.get('/sushi', function(request, response){
-		Sushi.find(function(err, sushi){
-			var allSushi = {sushi: sushi};
-			response.render('home', allSushi)
+		// Sushi.find(function(err, sushi){
+		// 	var allSushi = {sushi: sushi};
+			response.render('home')
 		})
-	})
+	
 
 
 // //get 2 sushi by id
-app.get('/sushi/:id', function(request, response){
-     //return a specific sushi roll by id
-     var id = request.params.id;
-     Sushi.findById(id, function(err, sushiRoll){
-     	response.json(sushi);
-     })
- })
+// app.get('/sushi/:id', function(request, response){
+//      //return a specific sushi roll by id
+//      var id = request.params.id;
+//      Sushi.findById(id, function(err, sushiRoll){
+//      	response.json(sushi);
+//      })
+//  })
 
 // //post /sushi --> use ajax function to actually post it
 app.post('/sushi', function(request, response){
@@ -53,33 +53,33 @@ app.post('/sushi', function(request, response){
      
 })
 
-// //patch /wines/:id
-app.patch('/sushi/:id', function(request, response){
-      //update sushi id with the request body
-      var id = request.params.id;
-      Sushi.findById(id, function(err, sushiRoll){
-      	sushi.name = request.body.name;
-		sushi.type = request.body.type;
-		sushi.vegetarian = request.body.vegetarian;
-		sushi.cost = request.body.cost;
-      })
+// // //patch /wines/:id
+// app.patch('/sushi/:id', function(request, response){
+//       //update sushi id with the request body
+//       var id = request.params.id;
+//       Sushi.findById(id, function(err, sushiRoll){
+//       	sushi.name = request.body.name;
+// 		sushi.type = request.body.type;
+// 		sushi.vegetarian = request.body.vegetarian;
+// 		sushi.cost = request.body.cost;
+//       })
 
-      sushi.save();
+//       sushi.save();
 
-      response.json(sushi);
+//       response.json(sushi);
 
-})
+// })
 
-// //delete /sushi/:id
-app.delete('/sushi/:id', function(request, response){
-      //delete the sushi by id
-      var id = request.params.id;
-      Sushi.findById(id, function(err, sushiRoll){
-      	sushi.remove();
-      	response.json('success');
-      })
+// // //delete /sushi/:id
+// app.delete('/sushi/:id', function(request, response){
+//       //delete the sushi by id
+//       var id = request.params.id;
+//       Sushi.findById(id, function(err, sushiRoll){
+//       	sushi.remove();
+//       	response.json('success');
+//       })
 
-})
+// })
 
 
 //fire up server
